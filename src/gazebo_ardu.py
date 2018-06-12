d44a5b2f20b78ba673dc32595e527ec04439fb95
Pierre Kancir <pierre.kancir@azurdrones.com>
2018-06-12 11:16:05 +0200

    Get vehicle type by mavros diagnostic directly

--- src/gazebo_ardu.py
+++ src/gazebo_ardu.py
@@ -7,27 +7,27 @@ from gazebo_msgs.msg import ModelState
 import mavros
 from mavros_msgs.msg import State
 from mavros_msgs.srv import StreamRate, StreamRateRequest
+from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
 
 """
 ROS subscribes to mavros and publish pose data to gazebo model for visualization.
 NOT A SIMULATOR, JUST A VISUALIZATION
 """
-
+vehicle_type = ["Quadrotor", "Ground rover"]
 
 class GazeboArdu():
     def __init__(self):
         self.model_name = "iris_demo"
-        self.is_rover = False
+        self.type = "Quadrotor"
         self.state = False
         self.state_pub = rospy.Publisher("/gazebo/set_model_state", ModelState, queue_size=10)
         self.sub_pos = rospy.Subscriber("/mavros/local_position/pose", PoseStamped, self.pose_callback)
         self.sub_state = rospy.Subscriber("/mavros/state", State, self.state_callback)
+        self.vehicle_type_sub = None
 
 
         if rospy.has_param("~vehicle_name"):
             self.model_name = rospy.get_param("~vehicle_name")
-        if rospy.has_param("~is_rover"):
-            self.is_rover = rospy.get_param("~is_rover")
         self.model_state = ModelState()
         self.model_state.model_name = self.model_name
 
@@ -39,17 +39,28 @@ class GazeboArdu():
 
     def pose_callback(self, position):
         self.model_state.pose = position.pose
-        if self.is_rover:
+        if self.type == "Ground rover":
             self.model_state.pose.position.z = 0.1
         #if self.model_state.pose.position.z < 0.1:
         #    self.model_state.pose.position.z = 0.1
         self.state_pub.publish(self.model_state)
 
     def state_callback(self, state):
-        if self.state is not state:
-            self.state = state
+        if self.state is not state.connected:
+            self.state = state.connected
             set_rate = rospy.ServiceProxy(mavros.get_topic('set_stream_rate'), StreamRate)
             set_rate(stream_id=StreamRateRequest.STREAM_ALL, message_rate=20, on_off=True)
+            self.vehicle_type_sub = rospy.Subscriber("/diagnostics", DiagnosticArray, self.type_callback)
+
+
+    def type_callback(self, diag):
+        index1 = [diag.status.index(x) for x in diag.status if x.name == "mavros: Heartbeat"]
+        index2 = [diag.status[index1[0]].values.index(x) for x in diag.status[index1[0]].values if x.key == "Vehicle type"]
+        self.type = diag.status[index1[0]].values[index2[0]].value
+        if self.type in vehicle_type:
+            self.vehicle_type_sub.unregister()
+
+
 
 if __name__ == '__main__':
     rospy.init_node("GazeboArdu", anonymous=True)
